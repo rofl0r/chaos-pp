1ca6fc84256228c1f2dadba8789ac536958e033f
pmenso57 <pmenso57>
2003-09-22 12:43:19 +0000

    initial

---
+++ chaos/preprocessor/wstringize.h
@@ -0,0 +1,54 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_WSTRINGIZE_H
+# define CHAOS_PREPROCESSOR_WSTRINGIZE_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_WSTRINGIZE */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_WSTRINGIZE(...) CHAOS_PP_PRIMITIVE_WSTRINGIZE(__VA_ARGS__)
+#    define CHAOS_PP_WSTRINGIZE_ CHAOS_PP_LAMBDA(CHAOS_PP_WSTRINGIZE)
+# else
+#    define CHAOS_PP_WSTRINGIZE(x) CHAOS_PP_PRIMITIVE_WSTRINGIZE(x)
+# endif
+#
+# define CHAOS_PP_WSTRINGIZE_ID() CHAOS_PP_WSTRINGIZE
+#
+# if CHAOS_PP_NO_PREFIX
+#    define WSTRINGIZE    CHAOS_PP_PREFIX(WSTRINGIZE)
+#    define WSTRINGIZE_   CHAOS_PP_PREFIX(WSTRINGIZE_)
+#    define WSTRINGIZE_ID CHAOS_PP_PREFIX(WSTRINGIZE_ID)
+# endif
+#
+# /* CHAOS_PP_PRIMITIVE_WSTRINGIZE */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_PRIMITIVE_WSTRINGIZE(...) CHAOS_PP_PRIMITIVE_CAT(L, #__VA_ARGS__)
+#    define CHAOS_PP_PRIMITIVE_WSTRINGIZE_ CHAOS_PP_LAMBDA(CHAOS_PP_PRIMITIVE_WSTRINGIZE)
+# else
+#    define CHAOS_PP_PRIMITIVE_WSTRINGIZE(x) CHAOS_PP_PRIMITIVE_CAT(L, #x)
+# endif
+#
+# define CHAOS_PP_PRIMITIVE_WSTRINGIZE_ID() CHAOS_PP_PRIMITIVE_WSTRINGIZE
+#
+# if CHAOS_PP_NO_PREFIX
+#    define PRIMITIVE_WSTRINGIZE    CHAOS_PP_PREFIX(PRIMITIVE_WSTRINGIZE)
+#    define PRIMITIVE_WSTRINGIZE_   CHAOS_PP_PREFIX(PRIMITIVE_WSTRINGIZE_)
+#    define PRIMITIVE_WSTRINGIZE_ID CHAOS_PP_PREFIX(PRIMITIVE_WSTRINGIZE_ID)
+# endif
+#
+# endif

781ad51f16a98f1bf88a63173c56e366325e4c0a
pmenso57 <pmenso57>
2003-09-22 20:40:06 +0000

    initial

---
+++ chaos/preprocessor/logical/and.h
@@ -0,0 +1,39 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_LOGICAL_AND_H
+# define CHAOS_PREPROCESSOR_LOGICAL_AND_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/logical/bool.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_AND */
+#
+# define CHAOS_PP_AND(p) CHAOS_PP_CAT(CHAOS_IP_AND_, CHAOS_PP_BOOL(p))
+# define CHAOS_PP_AND_ID() CHAOS_PP_AND
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_AND_ CHAOS_PP_LAMBDA(CHAOS_PP_AND)
+# endif
+#
+# define CHAOS_IP_AND_0(q) 0
+# define CHAOS_IP_AND_1(q) CHAOS_PP_BOOL(q)
+#
+# if CHAOS_PP_NO_PREFIX
+#    define AND    CHAOS_PP_PREFIX(AND)
+#    define AND_   CHAOS_PP_PREFIX(AND_)
+#    define AND_ID CHAOS_PP_PREFIX(AND_ID)
+# endif
+#
+# endif
---
+++ chaos/preprocessor/logical/bitand.h
@@ -0,0 +1,38 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_LOGICAL_BITAND_H
+# define CHAOS_PREPROCESSOR_LOGICAL_BITAND_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_BITAND */
+#
+# define CHAOS_PP_BITAND(x) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_BITAND_, x)
+# define CHAOS_PP_BITAND_ID() CHAOS_PP_BITAND
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_BITAND_ CHAOS_PP_LAMBDA(CHAOS_PP_BITAND)
+# endif
+#
+# define CHAOS_IP_BITAND_0(y) 0
+# define CHAOS_IP_BITAND_1(y) y
+#
+# if CHAOS_PP_NO_PREFIX
+#    define BITAND    CHAOS_PP_PREFIX(BITAND)
+#    define BITAND_   CHAOS_PP_PREFIX(BITAND_)
+#    define BITAND_ID CHAOS_PP_PREFIX(BITAND_ID)
+# endif
+#
+# endif

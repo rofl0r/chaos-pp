bbd892609786e15b06524d8214aa62dab7591d42
pmenso57 <pmenso57>
2003-09-22 21:18:02 +0000

    initial

---
+++ chaos/preprocessor/tuple/eat.h
@@ -0,0 +1,64 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_TUPLE_EAT_H
+# define CHAOS_PREPROCESSOR_TUPLE_EAT_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_TUPLE_EAT */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TUPLE_EAT(size) CHAOS_IP_TUPLE_EAT_I
+#    define CHAOS_PP_TUPLE_EAT_ CHAOS_PP_LAMBDA(CHAOS_PP_TUPLE_EAT)
+#    define CHAOS_IP_TUPLE_EAT_I(...)
+# else
+#    define CHAOS_PP_TUPLE_EAT(size) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_TUPLE_EAT_, size)
+#    define CHAOS_IP_TUPLE_EAT_0()
+#    define CHAOS_IP_TUPLE_EAT_1(a)
+#    define CHAOS_IP_TUPLE_EAT_2(a, b)
+#    define CHAOS_IP_TUPLE_EAT_3(a, b, c)
+#    define CHAOS_IP_TUPLE_EAT_4(a, b, c, d)
+#    define CHAOS_IP_TUPLE_EAT_5(a, b, c, d, e)
+#    define CHAOS_IP_TUPLE_EAT_6(a, b, c, d, e, f)
+#    define CHAOS_IP_TUPLE_EAT_7(a, b, c, d, e, f, g)
+#    define CHAOS_IP_TUPLE_EAT_8(a, b, c, d, e, f, g, h)
+#    define CHAOS_IP_TUPLE_EAT_9(a, b, c, d, e, f, g, h, i)
+#    define CHAOS_IP_TUPLE_EAT_10(a, b, c, d, e, f, g, h, i, j)
+#    define CHAOS_IP_TUPLE_EAT_11(a, b, c, d, e, f, g, h, i, j, k)
+#    define CHAOS_IP_TUPLE_EAT_12(a, b, c, d, e, f, g, h, i, j, k, l)
+#    define CHAOS_IP_TUPLE_EAT_13(a, b, c, d, e, f, g, h, i, j, k, l, m)
+#    define CHAOS_IP_TUPLE_EAT_14(a, b, c, d, e, f, g, h, i, j, k, l, m, n)
+#    define CHAOS_IP_TUPLE_EAT_15(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o)
+#    define CHAOS_IP_TUPLE_EAT_16(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p)
+#    define CHAOS_IP_TUPLE_EAT_17(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q)
+#    define CHAOS_IP_TUPLE_EAT_18(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r)
+#    define CHAOS_IP_TUPLE_EAT_19(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s)
+#    define CHAOS_IP_TUPLE_EAT_20(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t)
+#    define CHAOS_IP_TUPLE_EAT_21(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u)
+#    define CHAOS_IP_TUPLE_EAT_22(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v)
+#    define CHAOS_IP_TUPLE_EAT_23(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w)
+#    define CHAOS_IP_TUPLE_EAT_24(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x)
+#    define CHAOS_IP_TUPLE_EAT_25(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y)
+# endif
+#
+# define CHAOS_PP_TUPLE_EAT_ID() CHAOS_PP_TUPLE_EAT
+#
+# if CHAOS_PP_NO_PREFIX
+#    define TUPLE_EAT    CHAOS_PP_PREFIX(TUPLE_EAT)
+#    define TUPLE_EAT_   CHAOS_PP_PREFIX(TUPLE_EAT_)
+#    define TUPLE_EAT_ID CHAOS_PP_PREFIX(TUPLE_EAT_ID)
+# endif
+#
+# endif
---
+++ chaos/preprocessor/tuple/rem.h
@@ -0,0 +1,79 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_TUPLE_REM_H
+# define CHAOS_PREPROCESSOR_TUPLE_REM_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_TUPLE_REM */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TUPLE_REM(size) CHAOS_IP_TUPLE_REM_I
+#    define CHAOS_PP_TUPLE_REM_ CHAOS_PP_LAMBDA(CHAOS_PP_TUPLE_REM)
+#    define CHAOS_IP_TUPLE_REM_I(...) __VA_ARGS__
+# else
+#    define CHAOS_PP_TUPLE_REM(size) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_TUPLE_REM_, size)
+#    define CHAOS_IP_TUPLE_REM_0()
+#    define CHAOS_IP_TUPLE_REM_1(a) a
+#    define CHAOS_IP_TUPLE_REM_2(a, b) a, b
+#    define CHAOS_IP_TUPLE_REM_3(a, b, c) a, b, c
+#    define CHAOS_IP_TUPLE_REM_4(a, b, c, d) a, b, c, d
+#    define CHAOS_IP_TUPLE_REM_5(a, b, c, d, e) a, b, c, d, e
+#    define CHAOS_IP_TUPLE_REM_6(a, b, c, d, e, f) a, b, c, d, e, f
+#    define CHAOS_IP_TUPLE_REM_7(a, b, c, d, e, f, g) a, b, c, d, e, f, g
+#    define CHAOS_IP_TUPLE_REM_8(a, b, c, d, e, f, g, h) a, b, c, d, e, f, g, h
+#    define CHAOS_IP_TUPLE_REM_9(a, b, c, d, e, f, g, h, i) a, b, c, d, e, f, g, h, i
+#    define CHAOS_IP_TUPLE_REM_10(a, b, c, d, e, f, g, h, i, j) a, b, c, d, e, f, g, h, i, j
+#    define CHAOS_IP_TUPLE_REM_11(a, b, c, d, e, f, g, h, i, j, k) a, b, c, d, e, f, g, h, i, j, k
+#    define CHAOS_IP_TUPLE_REM_12(a, b, c, d, e, f, g, h, i, j, k, l) a, b, c, d, e, f, g, h, i, j, k, l
+#    define CHAOS_IP_TUPLE_REM_13(a, b, c, d, e, f, g, h, i, j, k, l, m) a, b, c, d, e, f, g, h, i, j, k, l, m
+#    define CHAOS_IP_TUPLE_REM_14(a, b, c, d, e, f, g, h, i, j, k, l, m, n) a, b, c, d, e, f, g, h, i, j, k, l, m, n
+#    define CHAOS_IP_TUPLE_REM_15(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o
+#    define CHAOS_IP_TUPLE_REM_16(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p
+#    define CHAOS_IP_TUPLE_REM_17(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q
+#    define CHAOS_IP_TUPLE_REM_18(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r
+#    define CHAOS_IP_TUPLE_REM_19(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s
+#    define CHAOS_IP_TUPLE_REM_20(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t
+#    define CHAOS_IP_TUPLE_REM_21(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u
+#    define CHAOS_IP_TUPLE_REM_22(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v
+#    define CHAOS_IP_TUPLE_REM_23(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w
+#    define CHAOS_IP_TUPLE_REM_24(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x
+#    define CHAOS_IP_TUPLE_REM_25(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y) a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y
+# endif
+#
+# define CHAOS_PP_TUPLE_REM_ID() CHAOS_PP_TUPLE_REM
+#
+# if CHAOS_PP_NO_PREFIX
+#    define TUPLE_REM    CHAOS_PP_PREFIX(TUPLE_REM)
+#    define TUPLE_REM_   CHAOS_PP_PREFIX(TUPLE_REM_)
+#    define TUPLE_REM_ID CHAOS_PP_PREFIX(TUPLE_REM_ID)
+# endif
+#
+# /* CHAOS_PP_TUPLE_REM_CTOR */
+#
+# define CHAOS_PP_TUPLE_REM_CTOR(size, tuple) CHAOS_PP_TUPLE_REM(size) tuple
+# define CHAOS_PP_TUPLE_REM_CTOR_ID() CHAOS_PP_TUPLE_REM_CTOR
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TUPLE_REM_CTOR_ CHAOS_PP_LAMBDA(CHAOS_PP_TUPLE_REM_CTOR_ID)()
+# endif
+#
+# if CHAOS_PP_NO_PREFIX
+#    define TUPLE_REM_CTOR    CHAOS_PP_PREFIX(TUPLE_REM_CTOR)
+#    define TUPLE_REM_CTOR_   CHAOS_PP_PREFIX(TUPLE_REM_CTOR_)
+#    define TUPLE_REM_CTOR_ID CHAOS_PP_PREFIX(TUPLE_REM_CTOR_ID)
+# endif
+#
+# endif

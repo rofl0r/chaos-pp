90ea84e82e28519fbf21eb7bef9edcbf77befec8
pmenso57 <pmenso57>
2003-09-22 12:50:40 +0000

    initial

---
+++ chaos/preprocessor/control/iif.h
@@ -0,0 +1,37 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_CONTROL_IIF_H
+# define CHAOS_PREPROCESSOR_CONTROL_IIF_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_IIF */
+#
+# define CHAOS_PP_IIF(bit) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_IIF_, bit)
+# define CHAOS_PP_IIF_ID() CHAOS_PP_IIF
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IIF_ CHAOS_PP_LAMBDA(CHAOS_PP_IIF)
+#    define CHAOS_IP_IIF_0(t, ...) __VA_ARGS__
+#    define CHAOS_IP_IIF_1(t, ...) t
+# endif
+#
+# if CHAOS_PP_NO_PREFIX
+#    define IIF    CHAOS_PP_PREFIX(IIF)
+#    define IIF_   CHAOS_PP_PREFIX(IIF_)
+#    define IIF_ID CHAOS_PP_PREFIX(IIF_ID)
+# endif
+#
+# endif
---
+++ chaos/preprocessor/facilities/split.h
@@ -0,0 +1,41 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_FACILITIES_SPLIT_H
+# define CHAOS_PREPROCESSOR_FACILITIES_SPLIT_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/names.h>
+#
+# /* CHAOS_PP_SPLIT */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_SPLIT(i, ...) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_SPLIT_, i)(__VA_ARGS__)
+#    define CHAOS_PP_SPLIT_ CHAOS_PP_LAMBDA(CHAOS_PP_SPLIT_ID)()
+#    define CHAOS_IP_SPLIT_0(a, ...) a
+#    define CHAOS_IP_SPLIT_1(a, ...) __VA_ARGS__
+# else
+#    define CHAOS_PP_SPLIT(i, im) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_SPLIT_, i)(im)
+#    define CHAOS_IP_SPLIT_0(a, b) a
+#    define CHAOS_IP_SPLIT_1(a, b) b
+# endif
+#
+# define CHAOS_PP_SPLIT_ID() CHAOS_PP_SPLIT
+#
+# if CHAOS_PP_NO_PREFIX
+#    define SPLIT    CHAOS_PP_PREFIX(SPLIT)
+#    define SPLIT_   CHAOS_PP_PREFIX(SPLIT_)
+#    define SPLIT_ID CHAOS_PP_PREFIX(SPLIT_ID)
+# endif
+#
+# endif

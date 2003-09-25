fb7bd7d92170b1824236815f07272b0c412a3d8c
pmenso57 <pmenso57>
2003-09-25 04:05:29 +0000

    initial

---
+++ chaos/preprocessor/generics/strip.h
@@ -0,0 +1,28 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_GENERICS_STRIP_H
+# define CHAOS_PREPROCESSOR_GENERICS_STRIP_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/tuple/eat.h>
+#
+# /* CHAOS_PP_STRIP */
+#
+# define CHAOS_PP_STRIP(g) CHAOS_PP_TUPLE_EAT(1) g
+# define CHAOS_PP_STRIP_ID() CHAOS_PP_STRIP
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_STRIP_ CHAOS_PP_LAMBDA(CHAOS_PP_STRIP_ID)()
+# endif
+#
+# endif
---
+++ chaos/preprocessor/generics/typeof.h
@@ -0,0 +1,30 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_GENERICS_TYPEOF_H
+# define CHAOS_PREPROCESSOR_GENERICS_TYPEOF_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/facilities/split.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_TYPEOF */
+#
+# define CHAOS_PP_TYPEOF(g) CHAOS_PP_SPLIT(0, CHAOS_IP_TYPEOF_I g)
+# define CHAOS_PP_TYPEOF_ID() CHAOS_PP_TYPEOF
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TYPEOF_ CHAOS_PP_LAMBDA(CHAOS_PP_TYPEOF)
+# endif
+#
+# define CHAOS_IP_TYPEOF_I(type) type, ~
+#
+# endif

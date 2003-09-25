dfe8f2a43216beab07d0ec2fdca380103d868a6e
pmenso57 <pmenso57>
2003-09-25 02:51:15 +0000

    initial

---
+++ chaos/preprocessor/limits.h
@@ -0,0 +1,19 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_LIMITS_H
+# define CHAOS_PREPROCESSOR_LIMITS_H
+#
+# /* CHAOS_PP_LIMIT_EXPR */
+#
+# define CHAOS_PP_LIMIT_EXPR 512
+#
+# endif
---
+++ chaos/preprocessor/recursion/basic.h
@@ -0,0 +1,59 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_RECURSION_BASIC_H
+# define CHAOS_PREPROCESSOR_RECURSION_BASIC_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/facilities/empty.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_DEFER */
+#
+# define CHAOS_PP_DEFER(macro) macro CHAOS_PP_EMPTY()
+# define CHAOS_PP_DEFER_ID() CHAOS_PP_DEFER
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_DEFER_ CHAOS_PP_LAMBDA(CHAOS_PP_DEFER)
+# endif
+#
+# /* CHAOS_PP_REFER */
+#
+# define CHAOS_PP_REFER(macro) macro
+# define CHAOS_PP_REFER_ID() CHAOS_PP_REFER
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_REFER_ CHAOS_PP_LAMBDA(CHAOS_PP_REFER)
+# endif
+#
+# /* CHAOS_PP_OBSTRUCT */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_OBSTRUCT(macro) macro CHAOS_PP_EMPTY CHAOS_PP_EMPTY()()
+#    define CHAOS_PP_OBSTRUCT_ CHAOS_PP_LAMBDA(CHAOS_PP_OBSTRUCT)
+# else
+#    define CHAOS_PP_OBSTRUCT() CHAOS_PP_EMPTY CHAOS_PP_EMPTY()()
+# endif
+#
+# define CHAOS_PP_OBSTRUCT_ID() CHAOS_PP_OBSTRUCT
+#
+# /* CHAOS_PP_UNPACK */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_UNPACK(...) __VA_ARGS__
+#    define CHAOS_PP_UNPACK_ CHAOS_PP_LAMBDA(CHAOS_PP_UNPACK_ID)()
+# else
+#    define CHAOS_PP_UNPACK(x) x
+# endif
+#
+# define CHAOS_PP_UNPACK_ID() CHAOS_PP_UNPACK
+#
+# endif

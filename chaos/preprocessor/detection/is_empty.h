d2afd7fe427be5f3021840c8aeeaa11dd6b6be94
pmenso57 <pmenso57>
2003-09-25 01:23:45 +0000

    initial

---
+++ chaos/preprocessor/detection/is_empty.h
@@ -0,0 +1,49 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_DETECTION_IS_EMPTY_H
+# define CHAOS_PREPROCESSOR_DETECTION_IS_EMPTY_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/detection/is_nullary.h>
+# include <chaos/preprocessor/detection/is_variadic.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_IS_EMPTY */
+#
+# define CHAOS_PP_IS_EMPTY(id) \
+    CHAOS_PP_IS_NULLARY(CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_IS_EMPTY_, id CHAOS_IP_IS_EMPTY_I)) \
+    /**/
+# define CHAOS_PP_IS_EMPTY_ID() CHAOS_PP_IS_EMPTY
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IS_EMPTY_ CHAOS_PP_LAMBDA(CHAOS_PP_IS_EMPTY)
+# endif
+#
+# define CHAOS_IP_IS_EMPTY_CHAOS_IP_IS_EMPTY_I ()
+#
+# /* CHAOS_PP_IS_EMPTY_NON_FUNCTION */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IS_EMPTY_NON_FUNCTION(...) \
+        CHAOS_PP_IIF(CHAOS_PP_IS_VARIADIC(__VA_ARGS__))( \
+            0, \
+            CHAOS_PP_IS_NULLARY(CHAOS_IP_IS_EMPTY_NON_FUNCTION_C __VA_ARGS__ ()) \
+        ) \
+        /**/
+#    define CHAOS_PP_IS_EMPTY_NON_FUNCTION_ID() CHAOS_PP_IS_EMPTY_NON_FUNCTION
+#    define CHAOS_PP_IS_EMPTY_NON_FUNCTION_ CHAOS_PP_LAMBDA(CHAOS_PP_IS_EMPTY_NON_FUNCTION)
+#    define CHAOS_IP_IS_EMPTY_NON_FUNCTION_C() ()
+# endif
+#
+# endif
---
+++ chaos/preprocessor/detection/is_flag.h
@@ -0,0 +1,35 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_DETECTION_IS_FLAG_H
+# define CHAOS_PREPROCESSOR_DETECTION_IS_FLAG_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/detection/is_empty.h>
+# include <chaos/preprocessor/detection/is_one.h>
+# include <chaos/preprocessor/facilities/empty.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_IS_FLAG */
+#
+# define CHAOS_PP_IS_FLAG(id) \
+    CHAOS_PP_IIF(CHAOS_PP_IS_EMPTY(id CHAOS_PP_EMPTY()))( \
+        1, CHAOS_PP_IS_ONE(id) \
+    ) \
+    /**/
+# define CHAOS_PP_IS_FLAG_ID() CHAOS_PP_IS_FLAG
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IS_FLAG_ CHAOS_PP_LAMBDA(CHAOS_PP_IS_FLAG)
+# endif
+#
+# endif

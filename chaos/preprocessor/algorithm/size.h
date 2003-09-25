0fec77d5db17f68089332f6c80447d753f3a1de5
pmenso57 <pmenso57>
2003-09-25 04:46:01 +0000

    initial

---
+++ chaos/preprocessor/algorithm/size.h
@@ -0,0 +1,54 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_ALGORITHM_SIZE_H
+# define CHAOS_PREPROCESSOR_ALGORITHM_SIZE_H
+#
+# include <chaos/preprocessor/arithmetic/inc.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/limits.h>
+# include <chaos/preprocessor/generics/type.h>
+# include <chaos/preprocessor/recursion/basic.h>
+# include <chaos/preprocessor/recursion/expr.h>
+#
+# /* CHAOS_PP_SIZE */
+#
+# define CHAOS_PP_SIZE(g) CHAOS_PP_SIZE_BYPASS(CHAOS_PP_LIMIT_EXPR, g)
+# define CHAOS_PP_SIZE_ID() CHAOS_PP_SIZE
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_SIZE_ CHAOS_PP_LAMBDA(CHAOS_PP_SIZE)
+# endif
+#
+# /* CHAOS_PP_SIZE_BYPASS */
+#
+# define CHAOS_PP_SIZE_BYPASS(s, g) \
+    CHAOS_PP_EXPR_S(s)(CHAOS_IP_SIZE_I(CHAOS_PP_OBSTRUCT(), CHAOS_PP_PREV(s), g, 0)) \
+    /**/
+# define CHAOS_PP_SIZE_BYPASS_ID() CHAOS_PP_SIZE_BYPASS
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_SIZE_BYPASS_ CHAOS_PP_LAMBDA(CHAOS_PP_SIZE_BYPASS)
+# endif
+#
+# define CHAOS_IP_SIZE_INDIRECT() CHAOS_IP_SIZE_I
+# define CHAOS_IP_SIZE_I(_, s, g, size) \
+    CHAOS_PP_IIF _(CHAOS_PP_IS_CONS(g))( \
+        CHAOS_PP_EXPR_S(s) _(CHAOS_IP_SIZE_INDIRECT _()( \
+            CHAOS_PP_OBSTRUCT _(), CHAOS_PP_PREV(s), CHAOS_PP_REST _(g), CHAOS_PP_INC(size) \
+        )), \
+        size \
+    ) \
+    /**/
+#
+# endif
---
+++ chaos/preprocessor/facilities/overload.h
@@ -0,0 +1,44 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_FACILITIES_OVERLOAD_H
+# define CHAOS_PREPROCESSOR_FACILITIES_OVERLOAD_H
+#
+# include <chaos/preprocessor/algorithm/size.h>
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/limits.h>
+# include <chaos/preprocessor/recursion/basic.h>
+# include <chaos/preprocessor/recursion/expr.h>
+# include <chaos/preprocessor/tuple/type.h>
+#
+# /* CHAOS_PP_OVERLOAD */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_OVERLOAD(pre, ...) CHAOS_PP_OVERLOAD_BYPASS(CHAOS_PP_LIMIT_EXPR, pre, __VA_ARGS__)
+#    define CHAOS_PP_OVERLOAD_ID() CHAOS_PP_OVERLOAD
+#    define CHAOS_PP_OVERLOAD_ CHAOS_PP_LAMBDA(CHAOS_PP_OVERLOAD_ID)()
+# endif
+#
+# /* CHAOS_PP_OVERLOAD_BYPASS */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_OVERLOAD_BYPASS(s, pre, ...) \
+        CHAOS_PP_DEFER( \
+            CHAOS_PP_CAT(pre, CHAOS_PP_SIZE_BYPASS(s, (CHAOS_PP_TUPLE) (__VA_ARGS__))) \
+        )(__VA_ARGS__) \
+        /**/
+#    define CHAOS_PP_OVERLOAD_BYPASS_ID() CHAOS_PP_OVERLOAD_BYPASS
+#    define CHAOS_PP_OVERLOAD_BYPASS_ CHAOS_PP_LAMBDA(CHAOS_PP_OVERLOAD_BYPASS_ID)()
+# endif
+#
+# endif

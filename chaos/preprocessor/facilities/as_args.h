e24e082f1b574e0cf0a96f99ba382b87e1dd10c4
pmenso57 <pmenso57>
2003-09-25 02:01:16 +0000

    initial

---
+++ chaos/preprocessor/facilities/as_args.h
@@ -0,0 +1,35 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_FACILITIES_AS_ARGS_H
+# define CHAOS_PREPROCESSOR_FACILITIES_AS_ARGS_H
+#
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/tuple/rem.h>
+#
+# /* CHAOS_PP_AS_ARGS */
+#
+# define CHAOS_PP_AS_ARGS(n) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_AS_ARGS_, n)
+# define CHAOS_PP_AS_ARGS_ID() CHAOS_PP_AS_ARGS
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_AS_ARGS_ CHAOS_PP_LAMBDA(CHAOS_PP_AS_ARGS)
+# endif
+#
+# define CHAOS_IP_AS_ARGS_1(x) (CHAOS_PP_TUPLE_REM_CTOR x)
+# define CHAOS_IP_AS_ARGS_2(a, x) (a, CHAOS_PP_TUPLE_REM_CTOR x)
+# define CHAOS_IP_AS_ARGS_3(a, b, x) (a, b, CHAOS_PP_TUPLE_REM_CTOR x)
+# define CHAOS_IP_AS_ARGS_4(a, b, c, x) (a, b, c, CHAOS_PP_TUPLE_REM_CTOR x)
+# define CHAOS_IP_AS_ARGS_5(a, b, c, d, x) (a, b, c, d, CHAOS_PP_TUPLE_REM_CTOR x)
+#
+# endif
---
+++ chaos/preprocessor/facilities/identity.h
@@ -0,0 +1,30 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_FACILITIES_IDENTITY_H
+# define CHAOS_PREPROCESSOR_FACILITIES_IDENTITY_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/facilities/empty.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_IDENTITY */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IDENTITY(...) __VA_ARGS__ CHAOS_PP_EMPTY
+#    define CHAOS_PP_IDENTITY_ CHAOS_PP_LAMBDA(CHAOS_PP_IDENTITY_ID)()
+# else
+#    define CHAOS_PP_IDENTITY(x) x CHAOS_PP_EMPTY
+# endif
+#
+# define CHAOS_PP_IDENTITY_ID() CHAOS_PP_IDENTITY
+#
+# endif

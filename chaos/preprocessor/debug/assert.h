4e3fb2933c0722f7f464227f52762c1a4c2c8086
pmenso57 <pmenso57>
2003-10-02 23:36:38 +0000

    initial

---
+++ chaos/preprocessor/debug/assert.h
@@ -0,0 +1,42 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_DEBUG_ASSERT_H
+# define CHAOS_PREPROCESSOR_DEBUG_ASSERT_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/expr_iif.h>
+# include <chaos/preprocessor/debug/failure.h>
+# include <chaos/preprocessor/facilities/empty.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/logical/not.h>
+#
+# /* CHAOS_PP_ASSERT */
+#
+# define CHAOS_PP_ASSERT(cond) CHAOS_PP_EXPR_IIF(CHAOS_PP_NOT(cond))(CHAOS_PP_FAILURE())
+# define CHAOS_PP_ASSERT_ID() CHAOS_PP_ASSERT
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_ASSERT_ CHAOS_PP_LAMBDA(CHAOS_PP_ASSERT)
+# endif
+#
+# /* CHAOS_PP_ASSERT_MSG */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_ASSERT_MSG(cond, ...) CHAOS_PP_EXPR_IIF(CHAOS_PP_NOT(cond))(__VA_ARGS__)
+#    define CHAOS_PP_ASSERT_MSG_ CHAOS_PP_LAMBDA(CHAOS_PP_ASSERT_MSG)
+# else
+#    define CHAOS_PP_ASSERT_MSG(cond, msg) CHAOS_PP_EXPR_IIF(CHAOS_PP_NOT(cond))(msg)
+# endif
+#
+# define CHAOS_PP_ASSERT_MSG_ID() CHAOS_PP_ASSERT_MSG
+#
+# endif
---
+++ chaos/preprocessor/debug/failure.h
@@ -0,0 +1,29 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_DEBUG_FAILURE_H
+# define CHAOS_PREPROCESSOR_DEBUG_FAILURE_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_FAILURE */
+#
+# define CHAOS_PP_FAILURE() CHAOS_IP_FAILURE_I(!)
+# define CHAOS_PP_FAILURE_ID() CHAOS_PP_FAILURE
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_FAILURE_ CHAOS_PP_LAMBDA(CHAOS_PP_FAILURE)
+# endif
+#
+# define CHAOS_IP_FAILURE_I()
+#
+# endif

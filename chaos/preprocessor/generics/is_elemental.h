f3d9bf32d854e2337a9310e8a5b7d59f58a96aba
pmenso57 <pmenso57>
2003-10-03 16:54:16 +0000

    initial

---
+++ chaos/preprocessor/generics/is_elemental.h
@@ -0,0 +1,34 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_GENERICS_IS_ELEMENTAL_H
+# define CHAOS_PREPROCESSOR_GENERICS_IS_ELEMENTAL_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/detection/is_variadic.h>
+# include <chaos/preprocessor/generics/type.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/logical/compl.h>
+#
+# /* CHAOS_PP_IS_ELEMENTAL */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_IS_ELEMENTAL(type) \
+        CHAOS_PP_COMPL(CHAOS_PP_IS_VARIADIC(CHAOS_PP_ITEM(type,))) \
+        /**/
+#    define CHAOS_PP_IS_ELEMENTAL_ CHAOS_PP_LAMBDA(CHAOS_PP_IS_ELEMENTAL)
+# else
+#    define CHAOS_PP_IS_ELEMENTAL(type) 1
+# endif
+#
+# define CHAOS_PP_IS_ELEMENTAL_ID() CHAOS_PP_IS_ELEMENTAL
+#
+# endif

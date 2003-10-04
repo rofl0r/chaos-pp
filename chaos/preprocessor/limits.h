3b6d6942c669d7feaa328bb4c8230efc84386733
pmenso57 <pmenso57>
2003-10-04 19:20:00 +0000

    added full bindings

--- chaos/preprocessor/limits.h
+++ chaos/preprocessor/limits.h
@@ -12,12 +12,25 @@
 # ifndef CHAOS_PREPROCESSOR_LIMITS_H
 # define CHAOS_PREPROCESSOR_LIMITS_H
 #
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
 # /* CHAOS_PP_LIMIT_EXPR */
 #
 # define CHAOS_PP_LIMIT_EXPR 512
+# define CHAOS_PP_LIMIT_EXPR_ID() CHAOS_PP_LIMIT_EXPR
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_LIMIT_EXPR_ CHAOS_PP_LAMBDA(CHAOS_PP_LIMIT_EXPR_ID)()
+# endif
 #
 # /* CHAOS_PP_LIMIT_MAG */
 #
 # define CHAOS_PP_LIMIT_MAG 512
+# define CHAOS_PP_LIMIT_MAG_ID() CHAOS_PP_LIMIT_MAG
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_LIMIT_MAG_ CHAOS_PP_LAMBDA(CHAOS_PP_LIMIT_MAG_ID)()
+# endif
 #
 # endif

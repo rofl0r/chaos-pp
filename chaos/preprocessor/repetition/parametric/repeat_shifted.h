9ed8cf1da41009047e1f605c4b38cc39077316d7
pmenso57 <pmenso57>
2003-10-10 23:54:49 +0000

    moved parametrics

--- chaos/preprocessor/repetition/parametric/repeat_shifted.h
+++ chaos/preprocessor/repetition/parametric/repeat_shifted.h
@@ -9,35 +9,29 @@
 #  *                                                                         *
 #  ************************************************************************* */
 #
-# ifndef CHAOS_PREPROCESSOR_REPETITION_REPEAT_H
-# define CHAOS_PREPROCESSOR_REPETITION_REPEAT_H
+# ifndef CHAOS_PREPROCESSOR_REPETITION_PARAMETRIC_REPEAT_SHIFTED_H
+# define CHAOS_PREPROCESSOR_REPETITION_PARAMETRIC_REPEAT_SHIFTED_H
 #
 # include <chaos/preprocessor/config.h>
 # include <chaos/preprocessor/facilities/empty.h>
 # include <chaos/preprocessor/lambda/ops.h>
-# include <chaos/preprocessor/repetition/delineate.h>
+# include <chaos/preprocessor/repetition/parametric/delineate_shifted.h>
 # include <chaos/preprocessor/recursion/expr.h>
 #
-# /* CHAOS_PP_REPEAT */
+# /* CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC */
 #
 # if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_REPEAT(count, macro, ...) CHAOS_PP_REPEAT_S(CHAOS_PP_STATE(), count, macro, __VA_ARGS__)
-#    define CHAOS_PP_REPEAT_ CHAOS_PP_LAMBDA(CHAOS_PP_REPEAT_ID)()
-# else
-#    define CHAOS_PP_REPEAT(count, macro, data) CHAOS_PP_REPEAT_S(CHAOS_PP_STATE(), count, macro, data)
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC(size, count, macro, ...) CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S(CHAOS_PP_STATE(), size, count, macro, __VA_ARGS__)
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_ID() CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_ CHAOS_PP_LAMBDA(CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_ID)()
 # endif
 #
-# define CHAOS_PP_REPEAT_ID() CHAOS_PP_REPEAT
-#
-# /* CHAOS_PP_REPEAT_S */
+# /* CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S */
 #
 # if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_REPEAT_S(s, count, macro, ...) CHAOS_PP_DELINEATE_S(s, count, CHAOS_PP_EMPTY, macro, __VA_ARGS__)
-#    define CHAOS_PP_REPEAT_S_ CHAOS_PP_LAMBDA(CHAOS_PP_REPEAT_S_ID)()
-# else
-#    define CHAOS_PP_REPEAT_S(s, count, macro, data) CHAOS_PP_DELINEATE_S(s, count, CHAOS_PP_EMPTY, macro, data)
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S(s, size, count, macro, ...) CHAOS_PP_DELINEATE_SHIFTED_PARAMETRIC_S(s, size, count, CHAOS_PP_EMPTY, macro, __VA_ARGS__)
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S_ID() CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S
+#    define CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S_ CHAOS_PP_LAMBDA(CHAOS_PP_REPEAT_SHIFTED_PARAMETRIC_S_ID)()
 # endif
-#
-# define CHAOS_PP_REPEAT_S_ID() CHAOS_PP_REPEAT_S
 #
 # endif

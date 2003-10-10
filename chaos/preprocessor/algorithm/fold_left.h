6bd4328f350c11d1646f239b3303e620f4284f28
pmenso57 <pmenso57>
2003-10-10 09:16:58 +0000

    moved parametrics

--- chaos/preprocessor/algorithm/fold_left.h
+++ chaos/preprocessor/algorithm/fold_left.h
@@ -12,18 +12,14 @@
 # ifndef CHAOS_PREPROCESSOR_ALGORITHM_FOLD_LEFT_H
 # define CHAOS_PREPROCESSOR_ALGORITHM_FOLD_LEFT_H
 #
-# include <chaos/preprocessor/comparison/not_equal.h>
 # include <chaos/preprocessor/config.h>
 # include <chaos/preprocessor/control/iif.h>
 # include <chaos/preprocessor/generics/spec.h>
 # include <chaos/preprocessor/generics/typeof.h>
 # include <chaos/preprocessor/lambda/call.h>
 # include <chaos/preprocessor/lambda/ops.h>
-# include <chaos/preprocessor/limits.h>
-# include <chaos/preprocessor/logical/bitand.h>
 # include <chaos/preprocessor/punctuation/comma.h>
 # include <chaos/preprocessor/recursion/basic.h>
-# include <chaos/preprocessor/recursion/buffer.h>
 # include <chaos/preprocessor/recursion/expr.h>
 # include <chaos/preprocessor/tuple/eat.h>
 #
@@ -60,51 +56,6 @@
     CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FOLD_LEFT_INDIRECT _()( \
         CHAOS_PP_NEXT(s), op, _o, type, CHAOS_PP_REST(g), \
         (_o()(s, op, CHAOS_PP_ITEM(type, CHAOS_PP_FIRST(g)) CHAOS_PP_COMMA() CHAOS_PP_UNPACK ps)) \
-    )) \
-    /**/
-#
-# /* CHAOS_PP_FOLD_LEFT_PARAMETRIC */
-#
-# if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC(size, op, g, ...) CHAOS_PP_FOLD_LEFT_PARAMETRIC_S(CHAOS_PP_STATE(), size, op, g, __VA_ARGS__)
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC_ CHAOS_PP_LAMBDA(CHAOS_PP_FOLD_LEFT_PARAMETRIC_ID)()
-# else
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC(size, op, g, state) CHAOS_PP_FOLD_LEFT_PARAMETRIC_S(CHAOS_PP_STATE(), size, op, g, state)
-# endif
-#
-# define CHAOS_PP_FOLD_LEFT_PARAMETRIC_ID() CHAOS_PP_FOLD_LEFT_PARAMETRIC
-#
-# if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC_S(s, size, op, g, ...) CHAOS_IP_FOLD_LEFT_PARAMETRIC_U(s, size, op, g, (__VA_ARGS__))
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC_S_ CHAOS_PP_LAMBDA(CHAOS_PP_FOLD_LEFT_PARAMETRIC_S_ID)()
-# else
-#    define CHAOS_PP_FOLD_LEFT_PARAMETRIC_S(s, size, op, g, state) CHAOS_IP_FOLD_LEFT_PARAMETRIC_U(s, size, op, g, (state))
-# endif
-#
-# define CHAOS_PP_FOLD_LEFT_PARAMETRIC_S_ID() CHAOS_PP_FOLD_LEFT_PARAMETRIC_S
-#
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_U(s, size, op, g, ps) CHAOS_IP_FOLD_LEFT_PARAMETRIC_I(CHAOS_PP_NEXT(s), CHAOS_PP_BUFFER(s, size), op, g, ps)
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_I(s, buffer, op, g, ps) CHAOS_IP_FOLD_LEFT_PARAMETRIC_II(s, s, buffer, buffer, op, CHAOS_PP_CALL(op), CHAOS_PP_TYPEOF(g), g, ps)
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_INDIRECT() CHAOS_IP_FOLD_LEFT_PARAMETRIC_II
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_II(s, o, buffer, shelf, op, _o, type, g, ps) \
-    CHAOS_PP_IIF(CHAOS_PP_BITAND(CHAOS_PP_IS_VALID(buffer))(CHAOS_PP_NOT_EQUAL(buffer, CHAOS_PP_LIMIT_EXPR)))( \
-        CHAOS_IP_FOLD_LEFT_PARAMETRIC_III, CHAOS_IP_FOLD_LEFT_PARAMETRIC_P \
-    )(s, o, buffer, shelf, op, _o, type, g, ps) \
-    /**/
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_III(s, o, buffer, shelf, op, _o, type, g, ps) \
-    CHAOS_PP_IIF(CHAOS_PP_IS_CONS(g))( \
-        CHAOS_IP_FOLD_LEFT_PARAMETRIC_IV, CHAOS_PP_UNPACK ps CHAOS_PP_TUPLE_EAT(10) \
-    )(CHAOS_PP_OBSTRUCT(), s, o, buffer, shelf, op, _o, type, g, ps) \
-    /**/
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_IV(_, s, o, buffer, shelf, op, _o, type, g, ps) \
-    CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FOLD_LEFT_PARAMETRIC_INDIRECT _()( \
-        CHAOS_PP_NEXT(s), o, CHAOS_PP_NEXT(buffer), shelf, op, _o, type, CHAOS_PP_REST(g), \
-        (_o()(s, op, CHAOS_PP_ITEM(type, CHAOS_PP_FIRST(g)) CHAOS_PP_COMMA() CHAOS_PP_UNPACK ps)) \
-    )) \
-    /**/
-# define CHAOS_IP_FOLD_LEFT_PARAMETRIC_P(s, o, buffer, shelf, op, _o, type, g, ps) \
-    CHAOS_PP_EXPR_S CHAOS_PP_OBSTRUCT()(o)(CHAOS_IP_FOLD_LEFT_PARAMETRIC_INDIRECT CHAOS_PP_OBSTRUCT()()( \
-        CHAOS_PP_NEXT(o), o, CHAOS_PP_NEXT(shelf), shelf, op, _o, type, g, ps \
     )) \
     /**/
 #

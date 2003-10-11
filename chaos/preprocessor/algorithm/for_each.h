3d4adf12cb6f41a752e4691a3031ffa8fac39206
pmenso57 <pmenso57>
2003-10-11 00:59:10 +0000

    initial

---
+++ chaos/preprocessor/algorithm/for_each.h
@@ -0,0 +1,66 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_ALGORITHM_FOR_EACH_H
+# define CHAOS_PREPROCESSOR_ALGORITHM_FOR_EACH_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/generics/spec.h>
+# include <chaos/preprocessor/generics/typeof.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/lambda/trampoline.h>
+# include <chaos/preprocessor/punctuation/comma.h>
+# include <chaos/preprocessor/recursion/basic.h>
+# include <chaos/preprocessor/recursion/expr.h>
+# include <chaos/preprocessor/tuple/eat.h>
+#
+# /* CHAOS_PP_FOR_EACH */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_FOR_EACH(macro, g, ...) CHAOS_PP_FOR_EACH_S(CHAOS_PP_STATE(), macro, g, __VA_ARGS__)
+#    define CHAOS_PP_FOR_EACH_ CHAOS_PP_LAMBDA(CHAOS_PP_FOR_EACH_ID)()
+# else
+#    define CHAOS_PP_FOR_EACH(macro, g, data) CHAOS_PP_FOR_EACH_S(CHAOS_PP_STATE(), macro, g, data)
+# endif
+#
+# define CHAOS_PP_FOR_EACH_ID() CHAOS_PP_FOR_EACH
+#
+# /* CHAOS_PP_FOR_EACH_S */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_FOR_EACH_S(s, macro, g, ...) CHAOS_IP_FOR_EACH_U(s, macro, g, (__VA_ARGS__))
+#    define CHAOS_PP_FOR_EACH_S_ CHAOS_PP_LAMBDA(CHAOS_PP_FOR_EACH_S_ID)()
+# else
+#    define CHAOS_PP_FOR_EACH_S(s, macro, g, data) CHAOS_IP_FOR_EACH_U(s, macro, g, (data))
+# endif
+#
+# define CHAOS_PP_FOR_EACH_S_ID() CHAOS_PP_FOR_EACH_S
+#
+# define CHAOS_IP_FOR_EACH_U(s, macro, g, pd) \
+    CHAOS_PP_EXPR_S(s)(CHAOS_IP_FOR_EACH_I( \
+        CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(s), macro, CHAOS_PP_TRAMPOLINE(macro), CHAOS_PP_TYPEOF(g), g, pd \
+    )) \
+    /**/
+# define CHAOS_IP_FOR_EACH_INDIRECT() CHAOS_IP_FOR_EACH_I
+# define CHAOS_IP_FOR_EACH_I(s, o, macro, _m, type, g, pd) \
+    CHAOS_PP_IIF(CHAOS_PP_IS_CONS(g))( \
+        CHAOS_IP_FOR_EACH_II, CHAOS_PP_TUPLE_EAT(8) \
+    )(CHAOS_PP_OBSTRUCT(), s, o, macro, _m, type, g, pd) \
+    /**/
+# define CHAOS_IP_FOR_EACH_II(_, s, o, macro, _m, type, g, pd) \
+    _m()(s, o, macro, CHAOS_PP_ITEM(type, CHAOS_PP_FIRST(g)) CHAOS_PP_COMMA() CHAOS_PP_UNPACK pd) \
+    CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FOR_EACH_INDIRECT _()( \
+        CHAOS_PP_NEXT(s), o, macro, _m, type, CHAOS_PP_REST(g), pd \
+    )) \
+    /**/
+#
+# endif

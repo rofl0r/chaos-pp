008888a35b16333f76888057885542127ab66b40
pmenso57 <pmenso57>
2003-10-11 22:31:54 +0000

    initial

---
+++ chaos/preprocessor/recursion/delay.h
@@ -0,0 +1,34 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_RECURSION_DELAY_H
+# define CHAOS_PREPROCESSOR_RECURSION_DELAY_H
+#
+# include <chaos/preprocessor/arithmetic/dec.h>
+# include <chaos/preprocessor/control/if.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_DELAY */
+#
+# define CHAOS_PP_DELAY(count, macro) \
+    CHAOS_PP_IF_SHADOW(count)( \
+        CHAOS_PP_DELAY_ID CHAOS_PP_OBSTRUCT()()(CHAOS_PP_DEC(count), macro), \
+        macro \
+    ) \
+    /**/
+# define CHAOS_PP_DELAY_ID() CHAOS_PP_DELAY
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_DELAY_ CHAOS_PP_LAMBDA(CHAOS_PP_DELAY)
+# endif
+#
+# endif

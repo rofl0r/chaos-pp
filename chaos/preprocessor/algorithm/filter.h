43148cdbf8f28463533c3633b63fef6cec623c0e
pmenso57 <pmenso57>
2003-10-11 07:07:56 +0000

    planar variation

--- chaos/preprocessor/algorithm/filter.h
+++ chaos/preprocessor/algorithm/filter.h
@@ -14,9 +14,11 @@
 #
 # include <chaos/preprocessor/config.h>
 # include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/detection/is_variadic.h>
+# include <chaos/preprocessor/facilities/empty.h>
 # include <chaos/preprocessor/generics/spec.h>
 # include <chaos/preprocessor/generics/typeof.h>
-# include <chaos/preprocessor/lambda/call.h>
+# include <chaos/preprocessor/lambda/invoke.h>
 # include <chaos/preprocessor/lambda/ops.h>
 # include <chaos/preprocessor/punctuation/comma.h>
 # include <chaos/preprocessor/recursion/basic.h>
@@ -37,35 +39,41 @@
 # /* CHAOS_PP_FILTER_S */
 #
 # if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_FILTER_S(s, pred, g, ...) CHAOS_IP_FILTER_U(s, pred, g, (__VA_ARGS__))
+#    define CHAOS_PP_FILTER_S(s, pred, g, ...) CHAOS_IP_FILTER_U(s, pred, CHAOS_PP_IS_VARIADIC(pred), g, (__VA_ARGS__))
 #    define CHAOS_PP_FILTER_S_ CHAOS_PP_LAMBDA(CHAOS_PP_FILTER_S_ID)()
 # else
-#    define CHAOS_PP_FILTER_S(s, pred, g, data) CHAOS_IP_FILTER_U(s, pred, g, (data))
+#    define CHAOS_PP_FILTER_S(s, pred, g, data) CHAOS_IP_FILTER_U(s, pred, 0, g, (data))
 # endif
 #
 # define CHAOS_PP_FILTER_S_ID() CHAOS_PP_FILTER_S
 #
-# define CHAOS_IP_FILTER_U(s, pred, g, pd) \
-    CHAOS_IP_FILTER_I(CHAOS_PP_NEXT(s), pred, CHAOS_PP_CALL(pred), CHAOS_PP_TYPEOF(g), g, pd) \
+# define CHAOS_IP_FILTER_U(s, pred, v, g, pd) \
+    CHAOS_PP_EXPR_S(s)(CHAOS_IP_FILTER_I( \
+        CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(s), pred, \
+        CHAOS_PP_IIF(v)(CHAOS_PP_INVOKE_S, pred), CHAOS_PP_IIF(v)(pred CHAOS_PP_COMMA, CHAOS_PP_EMPTY), \
+        CHAOS_PP_TYPEOF(g), g, pd \
+    )) \
     /**/
 # define CHAOS_IP_FILTER_INDIRECT() CHAOS_IP_FILTER_I
-# define CHAOS_IP_FILTER_I(s, pred, _p, type, g, pd) \
+# define CHAOS_IP_FILTER_I(s, o, pred, _a, _b, type, g, pd) \
     CHAOS_PP_IIF(CHAOS_PP_IS_CONS(g))( \
-        CHAOS_IP_FILTER_II, CHAOS_PP_NIL(type) CHAOS_PP_TUPLE_EAT(7) \
-    )(CHAOS_PP_OBSTRUCT(), s, pred, _p, type, g, pd) \
+        CHAOS_IP_FILTER_II, CHAOS_PP_NIL(type) CHAOS_PP_TUPLE_EAT(9) \
+    )(CHAOS_PP_DEFER(CHAOS_PP_OBSTRUCT)(), s, o, pred, _a, _b, type, g, pd) \
     /**/
-# define CHAOS_IP_FILTER_II(_, s, pred, _p, type, g, pd) \
-    CHAOS_PP_IIF _(_p()(s, pred, CHAOS_PP_ITEM(type, CHAOS_PP_FIRST(g)) CHAOS_PP_COMMA() CHAOS_PP_UNPACK pd))( \
-        CHAOS_PP_CONS _( \
-            CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FILTER_INDIRECT _()( \
-                CHAOS_PP_NEXT(s), pred, _p, type, CHAOS_PP_REST _(g), pd \
-            )), \
-            CHAOS_PP_FIRST _(g) \
-        ), \
-        CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FILTER_INDIRECT _()( \
-            CHAOS_PP_NEXT(s), pred, _p, type, CHAOS_PP_REST _(g), pd \
-        )) \
+# define CHAOS_IP_FILTER_II(_, s, o, pred, _a, _b, type, g, pd) \
+    CHAOS_PP_IIF _(CHAOS_PP_EXPR_S _(o)(_a _(o, _b() CHAOS_PP_ITEM(type, CHAOS_PP_FIRST(g)), CHAOS_PP_UNPACK pd)))( \
+        CHAOS_PP_CONS, CHAOS_IP_FILTER_III \
+    )( \
+        CHAOS_PP_DEFER(CHAOS_PP_EXPR_S(s))(CHAOS_PP_DEFER(CHAOS_IP_FILTER_INDIRECT)()( \
+            CHAOS_PP_NEXT(s), o, pred, _a, _b, type, CHAOS_PP_REST(g), pd \
+        )), \
+        CHAOS_PP_FIRST(g) \
     ) \
     /**/
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_IP_FILTER_III(g, ...) g
+# else
+#    define CHAOS_IP_FILTER_III(g, x) g
+# endif
 #
 # endif

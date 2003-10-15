bfa2c251219ca326cee505beefaccf7972aaa912
pmenso57 <pmenso57>
2003-10-15 22:26:21 +0000

    merged C++/C99 implementations

--- chaos/preprocessor/control/loop.h
+++ chaos/preprocessor/control/loop.h
@@ -22,6 +22,7 @@
 # include <chaos/preprocessor/recursion/basic.h>
 # include <chaos/preprocessor/recursion/buffer.h>
 # include <chaos/preprocessor/recursion/expr.h>
+# include <chaos/preprocessor/tuple/rem.h>
 #
 # /* CHAOS_PP_LOOP */
 #
@@ -37,44 +38,33 @@
 # /* CHAOS_PP_LOOP_S */
 #
 # if CHAOS_PP_VARIADICS
-#    define CHAOS_PP_LOOP_S(s, size, pred, op, ...) \
-        CHAOS_IP_LOOP_I(CHAOS_PP_OBSTRUCT(), CHAOS_PP_NEXT(s), CHAOS_PP_BUFFER(s, size), pred, CHAOS_PP_CALL(pred), op, CHAOS_PP_CALL(op), __VA_ARGS__) \
-        /**/
+#    define CHAOS_PP_LOOP_S(s, size, pred, op, ...) CHAOS_PP_DEFER(CHAOS_PP_TUPLE_REM_CTOR)(?, CHAOS_IP_LOOP_U(s, size, pred, op, (__VA_ARGS__)))
 #    define CHAOS_PP_LOOP_S_ CHAOS_PP_LAMBDA(CHAOS_PP_LOOP_S_ID)()
-#    define CHAOS_IP_LOOP_I(_, s, buffer, pred, _p, op, _o, ...) \
-        CHAOS_PP_IIF _( \
-            CHAOS_PP_BITAND _(CHAOS_PP_NOT_EQUAL(buffer, CHAOS_PP_LIMIT_EXPR))(_p()(s, pred, __VA_ARGS__)) \
-        )( \
-            CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
-                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, \
-                CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
-                    CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, _o()(s, op, __VA_ARGS__) \
-                )) \
-            )), \
-            __VA_ARGS__ \
-        ) \
-        /**/
 # else
-#    define CHAOS_PP_LOOP_S(s, size, pred, op, state) \
-        CHAOS_IP_LOOP_I(CHAOS_PP_OBSTRUCT(), CHAOS_PP_NEXT(s), CHAOS_PP_BUFFER(s, size), pred, CHAOS_PP_CALL(pred), op, CHAOS_PP_CALL(op), state) \
-        /**/
-#    define CHAOS_IP_LOOP_I(_, s, buffer, pred, _p, op, _o, state) \
-        CHAOS_PP_IIF _( \
-            CHAOS_PP_BITAND _(CHAOS_PP_NOT_EQUAL(buffer, CHAOS_PP_LIMIT_EXPR))(_p()(s, pred, state)) \
-        )( \
-            CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
-                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, \
-                CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
-                    CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, _o()(s, op, state) \
-                )) \
-            )), \
-            state \
-        ) \
-        /**/
+#    define CHAOS_PP_LOOP_S(s, size, pred, op, state) CHAOS_PP_DEFER(CHAOS_PP_TUPLE_REM_CTOR)(1, CHAOS_IP_LOOP_U(s, size, pred, op, (state)))
 # endif
 #
 # define CHAOS_PP_LOOP_S_ID() CHAOS_PP_LOOP_S
 #
+# define CHAOS_IP_LOOP_U(s, size, pred, op, ps) \
+    CHAOS_IP_LOOP_I( \
+        CHAOS_PP_OBSTRUCT(), CHAOS_PP_NEXT(s), CHAOS_PP_BUFFER(s, size), \
+        pred, CHAOS_PP_CALL(pred), op, CHAOS_PP_CALL(op), ps \
+    ) \
+    /**/
 # define CHAOS_IP_LOOP_INDIRECT() CHAOS_IP_LOOP_I
+# define CHAOS_IP_LOOP_I(_, s, buffer, pred, _p, op, _o, ps) \
+    CHAOS_PP_IIF _( \
+        CHAOS_PP_BITAND _(CHAOS_PP_NOT_EQUAL(buffer, CHAOS_PP_LIMIT_EXPR))(_p()(s, pred, CHAOS_PP_UNPACK ps)) \
+    )( \
+        CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
+            CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, \
+            CHAOS_PP_EXPR_S(s) _(CHAOS_IP_LOOP_INDIRECT _()( \
+                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), CHAOS_PP_NEXT(buffer), pred, _p, op, _o, (_o()(s, op, CHAOS_PP_UNPACK ps)) \
+            )) \
+        )), \
+        ps \
+    ) \
+    /**/
 #
 # endif

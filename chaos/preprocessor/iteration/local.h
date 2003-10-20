8d951e13db0d84b02b2e6e1519cc2cff740ec116
pmenso57 <pmenso57>
2003-10-20 23:43:50 +0000

    initial

---
+++ chaos/preprocessor/iteration/local.h
@@ -0,0 +1,37 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# ifndef CHAOS_PREPROCESSOR_ITERATION_LOCAL_H
+# define CHAOS_PREPROCESSOR_ITERATION_LOCAL_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/facilities/split.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/tuple/eat.h>
+# include <chaos/preprocessor/tuple/rem.h>
+#
+# /* CHAOS_PP_LOCAL_ITERATE */
+#
+# define CHAOS_PP_LOCAL_ITERATE() "chaos/preprocessor/iteration/detail/local.h"
+# define CHAOS_PP_LOCAL_ITERATE_ID() CHAOS_PP_LOCAL_ITERATE
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_LOCAL_ITERATE_ CHAOS_PP_LAMBDA(CHAOS_PP_LOCAL_ITERATE)
+# endif
+#
+# define CHAOS_IP_LOCAL_ITERATE_F(x, y) \
+    (CHAOS_PP_SPLIT(0, CHAOS_PP_TUPLE_REM_CTOR(2, CHAOS_PP_LOCAL_LIMITS))) <= x && x <= (CHAOS_PP_SPLIT(1, CHAOS_PP_TUPLE_REM_CTOR(2, CHAOS_PP_LOCAL_LIMITS))) \
+    /**/
+# define CHAOS_IP_LOCAL_ITERATE_R(x, y) \
+    (CHAOS_PP_SPLIT(0, CHAOS_PP_TUPLE_REM_CTOR(2, CHAOS_PP_LOCAL_LIMITS))) >= y && y >= (CHAOS_PP_SPLIT(1, CHAOS_PP_TUPLE_REM_CTOR(2, CHAOS_PP_LOCAL_LIMITS))) \
+    /**/
+#
+# endif

c6f5a7f222134859136ecf80c787c6b2844dcc87
pmenso57 <pmenso57>
2003-10-22 07:02:05 +0000

    initial

---
+++ chaos/preprocessor/iteration/detail/i6.h
@@ -0,0 +1,12 @@
+# /* *************************************************************************
+#  *                                                                         *
+#  *    (C) Copyright Paul Mensonides 2003.                                  *
+#  *                                                                         *
+#  *    Use, modification, and distribution are subject to Version 1.0 of    *
+#  *    the Boost Software License.  (See accompanying file LICENSE.)        *
+#  *                                                                         *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.         *
+#  *                                                                         *
+#  ************************************************************************* */
+#
+# error chaos-pp: iteration overflow

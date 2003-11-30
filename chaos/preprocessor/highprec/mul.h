c7dcf945eff9f5d87ca82e8af04fc60cab5a58e2
pmenso57 <pmenso57>
2003-11-30 05:49:27 +0000

    removed tabs

--- chaos/preprocessor/highprec/mul.h
+++ chaos/preprocessor/highprec/mul.h
@@ -75,14 +75,14 @@
 # define CHAOS_IP_MUL_HP_II( \
         o, _, \
         a9, a8, a7, a6, a5, a4, a3, a2, a1, a0, b9, b8, b7, b6, b5, b4, b3, b2, b1, b0, \
-	    c9, c8, c7, c6, c5, c4, c3, c2, c1, c0, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0, \
-	    e9, e8, e7, e6, e5, e4, e3, e2, e1, e0, f9, f8, f7, f6, f5, f4, f3, f2, f1, f0, \
-	    g9, g8, g7, g6, g5, g4, g3, g2, g1, g0, h9, h8, h7, h6, h5, h4, h3, h2, h1, h0, \
-	    i9, i8, i7, i6, i5, i4, i3, i2, i1, i0, j9, j8, j7, j6, j5, j4, j3, j2, j1, j0 \
-	) \
-	CHAOS_IP_MUL_HP_OVERFLOW_L2( \
-	    CHAOS_IP_MUL_HP_OVERFLOW_L2_ \
-		## b9 ## c9 ## c8 ## d9 ## d8 ## d7 ## e9 ## e8 ## e7 \
+        c9, c8, c7, c6, c5, c4, c3, c2, c1, c0, d9, d8, d7, d6, d5, d4, d3, d2, d1, d0, \
+        e9, e8, e7, e6, e5, e4, e3, e2, e1, e0, f9, f8, f7, f6, f5, f4, f3, f2, f1, f0, \
+        g9, g8, g7, g6, g5, g4, g3, g2, g1, g0, h9, h8, h7, h6, h5, h4, h3, h2, h1, h0, \
+        i9, i8, i7, i6, i5, i4, i3, i2, i1, i0, j9, j8, j7, j6, j5, j4, j3, j2, j1, j0 \
+    ) \
+    CHAOS_IP_MUL_HP_OVERFLOW_L2( \
+        CHAOS_IP_MUL_HP_OVERFLOW_L2_ \
+        ## b9 ## c9 ## c8 ## d9 ## d8 ## d7 ## e9 ## e8 ## e7 \
         ## e6 ## f9 ## f8 ## f7 ## f6 ## f5 ## g9 ## g8 ## g7 \
         ## g6 ## g5 ## g4 ## h9 ## h8 ## h7 ## h6 ## h5 ## h4 \
         ## h3 ## i9 ## i8 ## i7 ## i6 ## i5 ## i4 ## i3 ## i2 \
@@ -101,7 +101,7 @@
         ((a8)(_)(b7)(_)(c6)(_)(d5)(_)(e4)(_)(f3)(_)(g2)(_)(h1)(_)(i0))(o) \
         ((a9)(_)(b8)(_)(c7)(_)(d6)(_)(e5)(_)(f4)(_)(g3)(_)(h2)(_)(i1)(_)(j0))(CHAOS_PP_BOOL) \
     ) \
-	/**/
+    /**/
 # define CHAOS_IP_MUL_HP_III(im) (CHAOS_IP_MUL_HP_F_II(im))
 #
 # define CHAOS_IP_MUL_HP_OVERFLOW()

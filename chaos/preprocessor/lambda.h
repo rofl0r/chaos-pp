2dc6bf95c4fcf1381341f32a1f26e27d82502d36
pmenso57 <pmenso57>
2004-01-01 18:34:38 +0000

    renamed MERGE to CURRY

--- chaos/preprocessor/lambda.h
+++ chaos/preprocessor/lambda.h
@@ -14,9 +14,9 @@
 #
 # include <chaos/preprocessor/lambda/call.h>
 # include <chaos/preprocessor/lambda/closure.h>
+# include <chaos/preprocessor/lambda/curry.h>
 # include <chaos/preprocessor/lambda/execute.h>
 # include <chaos/preprocessor/lambda/invoke.h>
-# include <chaos/preprocessor/lambda/merge.h>
 # include <chaos/preprocessor/lambda/ops.h>
 # include <chaos/preprocessor/lambda/parse.h>
 # include <chaos/preprocessor/lambda/planar.h>

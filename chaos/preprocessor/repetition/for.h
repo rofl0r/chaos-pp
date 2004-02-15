da2dc7961c39af032c7ae6296c62c6c98e1ef505
pmenso57 <pmenso57>
2004-02-15 23:15:48 +0000

    allowed +2 trampolining

--- chaos/preprocessor/repetition/for.h
+++ chaos/preprocessor/repetition/for.h
@@ -13,12 +13,13 @@
 # define CHAOS_PREPROCESSOR_REPETITION_FOR_H
 #
 # include <chaos/preprocessor/config.h>
-# include <chaos/preprocessor/control/expr_iif.h>
 # include <chaos/preprocessor/control/iif.h>
 # include <chaos/preprocessor/lambda/call.h>
 # include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/lambda/trampoline.h>
 # include <chaos/preprocessor/recursion/basic.h>
 # include <chaos/preprocessor/recursion/expr.h>
+# include <chaos/preprocessor/tuple/eat.h>
 # include <chaos/preprocessor/tuple/rem.h>
 #
 # /* CHAOS_PP_FOR */
@@ -43,16 +44,23 @@
 #
 # define CHAOS_PP_FOR_S_ID() CHAOS_PP_FOR_S
 #
-# define CHAOS_IP_FOR_U(s, pred, op, macro, ps) \
-    CHAOS_IP_FOR_I(CHAOS_PP_OBSTRUCT(), CHAOS_PP_NEXT(s), pred, CHAOS_PP_CALL(pred), op, CHAOS_PP_CALL(op), macro, CHAOS_PP_CALL(macro), ps) \
+# define CHAOS_IP_FOR_U(s, pred, op, macro, ps) CHAOS_IP_FOR_I(CHAOS_PP_NEXT(s), pred, op, macro, ps)
+# define CHAOS_IP_FOR_I(s, pred, op, macro, ps) \
+    CHAOS_PP_DEFER(CHAOS_PP_EXPR_S)(s)(CHAOS_IP_FOR_II( \
+        CHAOS_PP_OBSTRUCT(), s, CHAOS_PP_NEXT(s), \
+        pred, CHAOS_PP_CALL(pred), op, CHAOS_PP_CALL(op), \
+        macro, CHAOS_PP_TRAMPOLINE(macro), ps \
+    )) \
     /**/
-# define CHAOS_IP_FOR_INDIRECT() CHAOS_IP_FOR_I
-# define CHAOS_IP_FOR_I(_, s, pred, _p, op, _o, macro, _m, ps) \
-    CHAOS_PP_EXPR_IIF _(_p()(s, pred, CHAOS_PP_UNPACK ps))( \
-        _m()(s, macro, CHAOS_PP_UNPACK ps) \
-        CHAOS_PP_EXPR_S(s) _(CHAOS_IP_FOR_INDIRECT _()( \
-            CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), pred, _p, op, _o, macro, _m, (_o()(s, op, CHAOS_PP_UNPACK ps)) \
-        )) \
+# define CHAOS_IP_FOR_INDIRECT() CHAOS_IP_FOR_II
+# define CHAOS_IP_FOR_II(_, s, o, pred, _p, op, _o, macro, _m, ps) \
+    CHAOS_PP_IIF _(_p()(s, pred, CHAOS_PP_UNPACK ps))( \
+        CHAOS_PP_EXPR_S(s), CHAOS_PP_TUPLE_EAT(1) \
+    )( \
+        _m _()(o, macro, CHAOS_PP_UNPACK ps) \
+        CHAOS_IP_FOR_INDIRECT _()( \
+            CHAOS_PP_OBSTRUCT _(), CHAOS_PP_NEXT(s), o, pred, _p, op, _o, macro, _m, (_o()(s, op, CHAOS_PP_UNPACK ps)) \
+        ) \
     ) \
     /**/
 #

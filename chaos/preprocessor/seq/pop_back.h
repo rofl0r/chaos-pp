5d4011d4ce0c2daefa6efa06318c8fca56c383cc
pmenso57 <pmenso57>
2004-04-27 00:35:27 +0000

    unlimited sequence size

--- chaos/preprocessor/seq/pop_back.h
+++ chaos/preprocessor/seq/pop_back.h
@@ -12,21 +12,43 @@
 # ifndef CHAOS_PREPROCESSOR_SEQ_POP_BACK_H
 # define CHAOS_PREPROCESSOR_SEQ_POP_BACK_H
 #
-# include <chaos/preprocessor/arithmetic/dec.h>
 # include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/expr_iif.h>
+# include <chaos/preprocessor/facilities/expand.h>
+# include <chaos/preprocessor/punctuation/paren.h>
 # include <chaos/preprocessor/lambda/ops.h>
-# include <chaos/preprocessor/seq/first_n.h>
-# include <chaos/preprocessor/seq/size.h>
+# include <chaos/preprocessor/seq/binary_transform.h>
 #
 # /* CHAOS_PP_SEQ_POP_BACK */
 #
 # define CHAOS_PP_SEQ_POP_BACK(seq) \
-    CHAOS_PP_SEQ_FIRST_N(CHAOS_PP_DEC(CHAOS_PP_SEQ_SIZE(seq)), seq) \
+    CHAOS_PP_EXPAND( \
+        CHAOS_IP_SEQ_POP_BACK_C CHAOS_PP_EXPAND( \
+            CHAOS_IP_SEQ_POP_BACK_A CHAOS_PP_SEQ_BINARY_TRANSFORM(seq, ~) \
+        ) \
+    ) \
     /**/
 # define CHAOS_PP_SEQ_POP_BACK_ID() CHAOS_PP_SEQ_POP_BACK
 #
 # if CHAOS_PP_VARIADICS
 #    define CHAOS_PP_SEQ_POP_BACK_ CHAOS_PP_LAMBDA(CHAOS_PP_SEQ_POP_BACK_ID)()
 # endif
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_IP_SEQ_POP_BACK_A(m, ...) ((__VA_ARGS__) CHAOS_IP_SEQ_POP_BACK_INDIRECT_1ST
+#    define CHAOS_IP_SEQ_POP_BACK_B(m, ...) \
+        , m) m(?)(() CHAOS_PP_LPAREN() (__VA_ARGS__) CHAOS_IP_SEQ_POP_BACK_INDIRECT_1ST) \
+        /**/
+# else
+#    define CHAOS_IP_SEQ_POP_BACK_A(m, x) ((x) CHAOS_IP_SEQ_POP_BACK_INDIRECT_1ST
+#    define CHAOS_IP_SEQ_POP_BACK_B(m, x) \
+        , m) m(1)(() CHAOS_PP_LPAREN() (x) CHAOS_IP_SEQ_POP_BACK_INDIRECT_1ST) \
+        /**/
+# endif
+#
+# define CHAOS_IP_SEQ_POP_BACK_C(e, m) m(1)(e CHAOS_IP_SEQ_POP_BACK_INDIRECT_2ND)
+#
+# define CHAOS_IP_SEQ_POP_BACK_INDIRECT_1ST() CHAOS_IP_SEQ_POP_BACK_B
+# define CHAOS_IP_SEQ_POP_BACK_INDIRECT_2ND() CHAOS_IP_SEQ_POP_BACK_C
 #
 # endif

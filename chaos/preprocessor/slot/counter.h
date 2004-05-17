c316f940d454d7b045d8120740485393aaba3cba
pmenso57 <pmenso57>
2004-05-17 15:21:36 +0000

    counter added

--- chaos/preprocessor/slot/counter.h
+++ chaos/preprocessor/slot/counter.h
@@ -9,10 +9,32 @@
 #  *                                                                         *
 #  ************************************************************************* */
 #
-# ifndef CHAOS_PREPROCESSOR_SLOT_H
-# define CHAOS_PREPROCESSOR_SLOT_H
+# ifndef CHAOS_PREPROCESSOR_SLOT_COUNTER_H
+# define CHAOS_PREPROCESSOR_SLOT_COUNTER_H
 #
-# include <chaos/preprocessor/slot/counter.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/extended/variadic_cat.h>
+# include <chaos/preprocessor/lambda/ops.h>
 # include <chaos/preprocessor/slot/slot.h>
+#
+# /* CHAOS_PP_COUNTER */
+#
+# define CHAOS_PP_COUNTER CHAOS_IP_COUNTER_I()
+# define CHAOS_PP_COUNTER_ID() CHAOS_PP_COUNTER
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_COUNTER_ CHAOS_PP_LAMBDA(CHAOS_PP_COUNTER_ID)()
+# endif
+#
+# define CHAOS_IP_COUNTER_I() 0
+#
+# /* CHAOS_PP_UPDATE_COUNTER */
+#
+# define CHAOS_PP_UPDATE_COUNTER() "chaos/preprocessor/slot/detail/counter.h"
+# define CHAOS_PP_UPDATE_COUNTER_ID() CHAOS_PP_UPDATE_COUNTER
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_UPDATE_COUNTER_ CHAOS_PP_LAMBDA(CHAOS_PP_UPDATE_COUNTER)
+# endif
 #
 # endif

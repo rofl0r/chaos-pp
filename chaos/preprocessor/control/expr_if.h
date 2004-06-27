699eb4ff5984d6ab7103e0ce29657da3300e42c0
pmenso57 <pmenso57>
2004-06-27 02:23:19 +0000

    EXPR_IF -> WHEN, EXPR_IIF -> INLINE_WHEN

---
+++ chaos/preprocessor/control/expr_if.h
@@ -0,0 +1,29 @@
+# /* ********************************************************************
+#  *                                                                    *
+#  *    (C) Copyright Paul Mensonides 2003-2004.                        *
+#  *                                                                    *
+#  *    Distributed under the Boost Software License, Version 1.0.      *
+#  *    (See accompanying file LICENSE).                                *
+#  *                                                                    *
+#  *    See http://chaos-pp.sourceforge.net for most recent version.    *
+#  *                                                                    *
+#  ******************************************************************** */
+#
+# ifndef CHAOS_PREPROCESSOR_CONTROL_EXPR_IF_H
+# define CHAOS_PREPROCESSOR_CONTROL_EXPR_IF_H
+#
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/expr_if.h>
+# include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/logical/bool.h>
+#
+# /* CHAOS_PP_EXPR_IF */
+#
+# define CHAOS_PP_EXPR_IF(cond) CHAOS_PP_EXPR_IIF(CHAOS_PP_BOOL(cond))
+# define CHAOS_PP_EXPR_IF_ID() CHAOS_PP_EXPR_IF
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_EXPR_IF_ CHAOS_PP_LAMBDA(CHAOS_PP_EXPR_IF)
+# endif
+#
+# endif

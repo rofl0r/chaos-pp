0560c710d00a0a8b8dc402b677c8f3c26ece1f5e
pmenso57 <pmenso57>
2004-07-27 05:51:22 +0000

    initial

--- chaos/preprocessor/control/placemarker_if.h
+++ chaos/preprocessor/control/placemarker_if.h
@@ -9,21 +9,24 @@
 #  *                                                                    *
 #  ******************************************************************** */
 #
-# ifndef CHAOS_PREPROCESSOR_CONTROL_H
-# define CHAOS_PREPROCESSOR_CONTROL_H
+# ifndef CHAOS_PREPROCESSOR_CONTROL_PLACEMARKER_IF_H
+# define CHAOS_PREPROCESSOR_CONTROL_PLACEMARKER_IF_H
 #
-# include <chaos/preprocessor/control/branch.h>
-# include <chaos/preprocessor/control/if.h>
-# include <chaos/preprocessor/control/iif.h>
-# include <chaos/preprocessor/control/include_if.h>
-# include <chaos/preprocessor/control/inline_unless.h>
-# include <chaos/preprocessor/control/inline_when.h>
-# include <chaos/preprocessor/control/null.h>
-# include <chaos/preprocessor/control/placemarker_if.h>
-# include <chaos/preprocessor/control/unless.h>
-# include <chaos/preprocessor/control/when.h>
-# include <chaos/preprocessor/control/while.h>
-# include <chaos/preprocessor/control/while_aux.h>
-# include <chaos/preprocessor/control/x.h>
+# include <chaos/preprocessor/cat.h>
+# include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/lambda/ops.h>
+#
+# /* CHAOS_PP_PLACEMARKER_IF */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_PLACEMARKER_IF(p) CHAOS_PP_PRIMITIVE_CAT(CHAOS_IP_PLACEMARKER_IF_, p)
+#    define CHAOS_PP_PLACEMARKER_IF_ID() CHAOS_PP_PLACEMARKER_IF
+#    define CHAOS_PP_PLACEMARKER_IF_ CHAOS_PP_LAMBDA(CHAOS_PP_PLACEMARKER_IF)
+# endif
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_IP_PLACEMARKER_IF_(...) __VA_ARGS__
+#    define CHAOS_IP_PLACEMARKER_IF_0(...)
+# endif
 #
 # endif

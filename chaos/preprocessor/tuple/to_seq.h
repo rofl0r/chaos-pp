fa485521b659b948c162aa35a55961c9af9b8146
pmenso57 <pmenso57>
2004-07-31 19:44:57 +0000

    unrolled tuple conversions

--- chaos/preprocessor/tuple/to_seq.h
+++ chaos/preprocessor/tuple/to_seq.h
@@ -14,7 +14,14 @@
 #
 # include <chaos/preprocessor/cat.h>
 # include <chaos/preprocessor/config.h>
+# include <chaos/preprocessor/control/iif.h>
+# include <chaos/preprocessor/control/inline_when.h>
 # include <chaos/preprocessor/lambda/ops.h>
+# include <chaos/preprocessor/limits.h>
+# include <chaos/preprocessor/recursion/basic.h>
+# include <chaos/preprocessor/recursion/expr.h>
+# include <chaos/preprocessor/tuple/batch.h>
+# include <chaos/preprocessor/tuple/core.h>
 #
 # /* CHAOS_PP_TUPLE_TO_SEQ */
 #
@@ -51,5 +58,48 @@
 # define CHAOS_IP_TUPLE_TO_SEQ_23(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w) (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)(k)(l)(m)(n)(o)(p)(q)(r)(s)(t)(u)(v)(w)
 # define CHAOS_IP_TUPLE_TO_SEQ_24(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x) (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)(k)(l)(m)(n)(o)(p)(q)(r)(s)(t)(u)(v)(w)(x)
 # define CHAOS_IP_TUPLE_TO_SEQ_25(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y) (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)(k)(l)(m)(n)(o)(p)(q)(r)(s)(t)(u)(v)(w)(x)(y)
+#
+# /* CHAOS_PP_TUPLE_TO_SEQ_ALT */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT(tuple) CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS(CHAOS_PP_LIMIT_EXPR, tuple)
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT_ID() CHAOS_PP_TUPLE_TO_SEQ_ALT
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT_ CHAOS_PP_LAMBDA(CHAOS_PP_TUPLE_TO_SEQ_ALT_ID)()
+# endif
+#
+# /* CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS */
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS(s, tuple) \
+        CHAOS_PP_EXPR_S(s)(CHAOS_IP_TUPLE_TO_SEQ_ALT_0( \
+            CHAOS_PP_OBSTRUCT(), CHAOS_PP_PREV(s), tuple \
+        )) \
+        /**/
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS_ID() CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS
+#    define CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS_ CHAOS_PP_LAMBDA(CHAOS_PP_TUPLE_TO_SEQ_ALT_BYPASS_ID)()
+# endif
+#
+# if CHAOS_PP_VARIADICS
+#    define CHAOS_IP_TUPLE_TO_SEQ_ALT_INDIRECT(n) CHAOS_IP_TUPLE_TO_SEQ_ALT_ ## n
+#    define CHAOS_IP_TUPLE_TO_SEQ_ALT_0(_, s, tuple) \
+        CHAOS_PP_IIF _(CHAOS_PP_TUPLE_IS_BATCH(10, tuple))( \
+            CHAOS_PP_TUPLE_TO_SEQ _(10, CHAOS_PP_TUPLE_BATCH _(10, tuple)) \
+            CHAOS_PP_EXPR_S _(s)(CHAOS_IP_TUPLE_TO_SEQ_ALT_INDIRECT _(0)( \
+                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_PREV(s), CHAOS_PP_TUPLE_SANS_BATCH _(10, tuple) \
+            )), \
+            CHAOS_PP_EXPR_S _(s)(CHAOS_IP_TUPLE_TO_SEQ_ALT_INDIRECT _(1)( \
+                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_PREV(s), tuple \
+            )) \
+        ) \
+        /**/
+#    define CHAOS_IP_TUPLE_TO_SEQ_ALT_1(_, s, tuple) \
+        CHAOS_PP_INLINE_WHEN _(CHAOS_PP_TUPLE_IS_CONS(tuple))( \
+            (CHAOS_PP_TUPLE_FIRST _(tuple)) \
+            CHAOS_PP_EXPR_S _(s)(CHAOS_IP_TUPLE_TO_SEQ_ALT_INDIRECT _(1)( \
+                CHAOS_PP_OBSTRUCT _(), CHAOS_PP_PREV(s), CHAOS_PP_TUPLE_REST _(tuple) \
+            )) \
+        ) \
+        /**/
+# endif
 #
 # endif

68dbb847bfaaa945c9dfc0a6d3f6912b9156db30
pmenso57 <pmenso57>
2005-09-15 08:16:30 +0000

    bug fixes galore + seeding

--- chaos/preprocessor/slot/counter.h
+++ chaos/preprocessor/slot/counter.h
@@ -14,19 +14,26 @@
 #
 # include <chaos/preprocessor/config.h>
 # include <chaos/preprocessor/extended/variadic_cat.h>
+# include <chaos/preprocessor/facilities/expand.h>
 # include <chaos/preprocessor/lambda/ops.h>
 # include <chaos/preprocessor/slot/slot.h>
 #
 # /* CHAOS_PP_COUNTER */
 #
-# define CHAOS_PP_COUNTER CHAOS_IP_COUNTER_I()
-# define CHAOS_PP_COUNTER_ID() CHAOS_PP_COUNTER
-#
 # if CHAOS_PP_VARIADICS
+#    define CHAOS_PP_COUNTER CHAOS_IP_COUNTER_I()
 #    define CHAOS_PP_COUNTER_ CHAOS_PP_LAMBDA(CHAOS_PP_COUNTER_ID)()
+# else
+#    define CHAOS_PP_COUNTER CHAOS_PP_EXPAND(CHAOS_IP_COUNTER_I())
 # endif
 #
-# define CHAOS_IP_COUNTER_I() 0
+# define CHAOS_PP_COUNTER_ID() CHAOS_PP_COUNTER
+#
+# ifndef CHAOS_PP_SEED
+#    define CHAOS_PP_SEED 0
+# endif
+#
+# define CHAOS_IP_COUNTER_I() CHAOS_PP_SEED
 #
 # /* CHAOS_PP_UPDATE_COUNTER */
 #
